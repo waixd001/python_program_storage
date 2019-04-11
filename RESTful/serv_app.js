96ed3f5dea287982e4853c73119962cdda6b6ed2
arWai <waixd001@gmail.com>
2019-04-11 21:26:48 +0800

    Internet program design Homework, Finished @ 0411 21:26

--- RESTful/serv_app.js
+++ RESTful/serv_app.js
@@ -20,17 +20,19 @@ app.get('/', function (req, res) {
  
 
 app.get('/listUsers', function (req, res) {
+   console.log('listUsers:');
    fs.readFile("./" + "users.json", 'utf8', function (err, data) {
        console.log( data );
        res.end( data );
    });
 })
 
-app.get('/listUser/:uuid', function (req, res) {
+app.get('/listUser', function (req, res) {
+   console.log('ID: '+req.query.uuid);
    // First read existing users.
    fs.readFile( "./" + "users.json", 'utf8', function (err, data) {
        users = JSON.parse( data );
-       var user = users["user" + req.params.uuid];
+       var user = users["user" + req.query.uuid];
        console.log( user );
        res.end( JSON.stringify(user));
    });
@@ -70,28 +72,53 @@ app.post('/addUser', function (req, res) {
 });
 
 app.put('/updateUser', function (req, res) {
-     var uuid  = req.body.uuid;
-     var uname = req.body.uname;
-     var prof  = req.body.prof;
-     var passwd = req.body.passwd;
-     var str = 'UPDATE: Name: '+ uname +', PROF: '+prof+' UUID: '+uuid+' PASS: '+ passwd;
+      var uuid  = req.body.uuid;
+      var uname = req.body.uname;
+      var prof  = req.body.prof;
+      var passwd = req.body.passwd;
+      var str = 'UPDATE: Name: '+ uname +', PROF: '+prof+' UUID: '+uuid+' PASS: '+ passwd;
 
-     users = JSON.parse( data );
-     var user = users["user" + uuid];
-     console.log( user );
+     // json data
+     var response = {
+      "name":uname,
+      "password":passwd,
+      "profession":prof,
+      "id": uuid
+     };
+     // name in json
+     var user = "user" + uuid;
+     users[user] = response;
+     // stringify to a good looking table
+     var input_data = JSON.stringify(users,null,4)
+
+     console.log(users); //console log checking
+
+     // write in
+     fs.writeFile('users.json', input_data , 'utf8',function (err) {
+      if (err)
+          console.log(err);
+      else
+          console.log('Write operation complete. ( ' + str + ' )');
+     });
      
-     if (user == NULL){
-        console.log('User not found');
-     }else{
-        console.log(str);
-     }
-
      console.log( str );
      res.end(str);
 })
 
-app.delete('/deleteUser/:id', function (req, res) {
-
+app.delete('/deleteUser', function (req, res) {
+   obj_id = req.query.uuid;
+   console.log(obj_id)
+   users = JSON.parse( data );
+   delete users['user'+ obj_id];
+   var input_data = JSON.stringify(users,null,4)
+   fs.writeFile('users.json', input_data , 'utf8',function (err) {
+      if (err)
+          console.log(err);
+      else
+          console.log('Delete operation complete.');
+     });
+   console.log(users);
+   res.end('deleted User');
 })
 
 var server = app.listen(8081, function () {
