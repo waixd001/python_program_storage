29cc3f9b6abe6e3c7ad6864d6ee285a29d70ec77
arWai <waixd001@gmail.com>
2019-05-16 00:13:04 +0800

    Update: Now the program will take sreenshot of full page

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -10,5 +10,10 @@ driver.get("https://www.cwb.gov.tw/V8/C/P/Rainfall/Rainfall_10Min_County.html")
 
 select = Select(driver.find_element_by_xpath("//select[@id='scity']"))
 select.select_by_visible_text("新北市")
-driver.save_screenshot("screenshot.png")
-driver.close()
+
+el = driver.find_element_by_tag_name('body')
+el.screenshot('screenshot.png')
+driver.quit()
+
+#driver.save_screenshot("screenshot.png")
+#driver.close()
